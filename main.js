f9a15fd8bed15522f73394e2f69a3565901b85b2
Michael Than <60087485+michaelthan24@users.noreply.github.com>
2020-09-08 23:18:02 -0700

    almost final

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ var timer = 256
 var tickRate = 16
 var visualRate = 256
 var msg = false
-var resources = {"cash":150,"beers":0,"BTquality":1,"manager":0,"barLevel":1,"bartender": 0}
+var count = 0
+
+var resources = {"cash":150,"beers":0,"BTquality":1,"manager":0,"barLevel":1,"bartender": 0,"beerSold":0}
 var costs = {"BTquality":15,
 	     "bartender":200,
-	     "bartender_tip":15,"beer_cost": 100,"manager_cost":1000,"bar_level":50000}
-var growthRate = {"BTquality":1.01,
-		  "bartender":1.05,
-	     "bartender_tip":1.15,"manager":1.10} 
+	     "bartender_tip":15,"beer_cost": 100,"manager_cost":1000,"bar_level":15000}
+var growthRate = {"BTquality":1.20,
+		  "bartender":1.20,
+	     "bartender_tip":1.15,"manager":1.20} 
 
 var increments = [{"input":["bartender","bartender_tip"],
 		   "output":"cash"}]
@@ -21,6 +23,7 @@ function serveDrinks(num){
     if (resources["beers"] > 0) {
     resources["cash"] += num*resources["BTquality"]
     resources["beers"] = resources["beers"] - 1
+    resources["beerSold"] += 1
     updateText()
     }
 };
@@ -37,7 +40,6 @@ function upgradeBartender(num){
     if (resources["cash"] >= costs["bartender_tip"]*num){
 	resources["bartender_tip"] += num
 	resources["cash"] -= num*costs["bartender_tip"]
-	
 	costs["bartender_tip"] *= growthRate["bartender_tip"]
 	document.getElementById("message").innerHTML = "Your bartenders love you as a boss for giving them an extra bonus. They are working extra hard for you now!"
     
@@ -47,7 +49,7 @@ function upgradeBartender(num){
 
 
 function upgradeBTquality(num){
-    if (resources["cash"] >= costs["BTquality"]*num){
+    if (resources["cash"] >= costs["BTquality"]*num){     
 	resources["BTquality"] += num
 	resources["cash"] -= num*costs["BTquality"]
 	
@@ -82,11 +84,10 @@ function hireBartender(num){
             if (!resources["bartender_tip"]){
 	           resources["bartender_tip"] = 1 
             }
+            count++
             resources["bartender"] += num
             resources["cash"] -= num*costs["bartender"]
-	
             costs["bartender"] *= growthRate["bartender"]
-	   
             document.getElementById("message").innerHTML = "You have just hired a new bartender. He will get right to work!"    
         
             updateText()
@@ -99,9 +100,17 @@ function hireBartender(num){
 
 function upgradeBar(num) {
     if (resources["cash"] >= costs["bar_level"]) {
-        resourcse["cash"] = resources["cash"] - costs["bar_level"]
-        resources["barLevel"] = resources["barLevel"] + num
-        updateText();
+        if (resources["manager"] >= resources["barLevel"]) {
+            resources["cash"] = resources["cash"] - costs["bar_level"]
+            resources["barLevel"] = resources["barLevel"] + num
+            costs["bar_level"] = costs["bar_level"] * 1.4
+            
+            updateText();
+            
+        }
+        else {
+            document.getElementById("message").innerHTML = "Cannot upgrade bar until you hire another manager!"
+        }
     }
 };
 
@@ -133,33 +142,53 @@ function updateText(){
 
 
 window.setInterval(function(){
-    timer += tickRate
-
-    
+    timer += tickRate    
     for (var increment of increments){
         total = 10
         for (var input of increment["input"]){
 	       total *= resources[input]  
         }
+        document.getElementById("beersold").innerHTML = "Beers sold: " + resources["beerSold"]
         document.getElementById("barLvl").innerHTML = "Bar level: " + resources["barLevel"]
         if (resources["manager"] == 0) {
             document.getElementById("inventPopup").style.display="block"
         }
         if (resources["manager"]==0 && msg==true && resources["beers"]==0) {
-            document.getElementById("message").innerHTML = "Buy a manager to have your bar always stocked up!"
+            document.getElementById("message").innerHTML = "Generate enough cash to hire a manager to have your bar always stocked up, and allow you to upgrade your bar level to make more ca$h!"
             }
-
         if (resources["manager"] >= 1 && resources["beers"] == 0) {
+            //add beer count here or...
             resources["cash"] = resources["cash"] - 100
             resources["beers"] = resources["beers"] + 200
         }
         if (total && resources["beers"] > 0){
+            //add beer count here
 	       console.log(total)
            resources["beers"] = resources["beers"] - 1
+           
+           resources["beerSold"] += count
 	       resources[increment["output"]] += total/tickRate
         }
         if (resources["beers"] > 0) {
             document.getElementById("inventPopup").style.display="none"
+        }
+        if (resources["beerSold"]==100) {
+            document.getElementById("message").innerHTML = "100 beers sold... better keep going you want to stay in business!"
+        }
+        if (resources["beerSold"]==1000) {
+            document.getElementById("message").innerHTML = "1000 beers sold... Not bad, but you are far from the talk of the town"
+        }
+        if (resources["beerSold"]==10000) {
+            document.getElementById("message").innerHTML = "10000 beers sold... Now you are starting to make some noise"
+        }
+        if (resources["beerSold"]==100000) {
+            document.getElementById("message").innerHTML = "100000 beers sold... You are beginning to draw celebrity attention!"
+        }
+        if (resources["beerSold"]==1000000) {
+            document.getElementById("message").innerHTML = "1000000 beers sold... You have the hottest bar in LA!"
+        }
+        if (resources["beerSold"]==10000000) {
+            document.getElementById("message").innerHTML = "10000000 beers sold, You are now keeping up with the Kardashians and Kanye!"
         }
     }
     
